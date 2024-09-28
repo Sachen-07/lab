ccdadc10eef7e52f7b431fa7045936864d061cb3
Sachen-07 <160990250+Sachen-07@users.noreply.github.com>
2024-09-28 21:31:11 +0545

    update

--- 8(vi).c
+++ 8(vi).c
@@ -7,7 +7,6 @@ int gd=DETECT,gm,points[]={100,200,120,230,150,260,120,290,100,330,80,290,50,260
 initgraph(&gd, &gm, "C:\\TurboC3\\BGI");
 setfillstyle(SOLID_FILL,10);
 fillpoly(9, points); 
-with color defined in setfillstyle function.
 getch();
 closegraph();
 return 0;
